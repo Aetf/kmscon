44a846b7b2a10a613cf874ddb526e095d4a4b2d1
Jocelyn Falempe <jfalempe@redhat.com>
2025-10-22 11:04:39 +0200

    Add rotation support to bblit renderer
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>

--- src/text_bblit.c
+++ src/text_bblit.c
@@ -36,14 +36,48 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include "font_rotate.h"
 #include "shl_log.h"
 #include "text.h"
 #include "uterm_video.h"
 
 #define LOG_SUBSYSTEM "text_bblit"
 
+struct bblit {
+	struct shl_hashtable *glyphs;
+	struct shl_hashtable *bold_glyphs;
+};
+
+static int bblit_init(struct kmscon_text *txt)
+{
+	struct bblit *bb;
+
+	bb = malloc(sizeof(*bb));
+	if (!bb)
+		return -ENOMEM;
+
+	txt->data = bb;
+	return 0;
+}
+
+static void bblit_destroy(struct kmscon_text *txt)
+{
+	struct bblit *bb = txt->data;
+
+	free(bb);
+}
+
+static void free_glyph(void *data)
+{
+	struct uterm_video_buffer *bb_glyph = data;
+
+	free(bb_glyph->data);
+	free(bb_glyph);
+}
+
 static int bblit_set(struct kmscon_text *txt)
 {
+	struct bblit *bb = txt->data;
 	unsigned int sw, sh;
 	struct uterm_mode *mode;
 
@@ -53,29 +87,49 @@ static int bblit_set(struct kmscon_text *txt)
 	sw = uterm_mode_get_width(mode);
 	sh = uterm_mode_get_height(mode);
 
-	txt->cols = sw / FONT_WIDTH(txt);
-	txt->rows = sh / FONT_HEIGHT(txt);
+	if (txt->orientation == OR_NORMAL || txt->orientation == OR_UPSIDE_DOWN) {
+		txt->cols = sw / FONT_WIDTH(txt);
+		txt->rows = sh / FONT_HEIGHT(txt);
+	} else {
+		txt->rows = sw / FONT_HEIGHT(txt);
+		txt->cols = sh / FONT_WIDTH(txt);
+	}
 
-	return 0;
+	return kmscon_rotate_create_tables(&bb->glyphs, &bb->bold_glyphs, free_glyph);
 }
 
-static int bblit_draw(struct kmscon_text *txt,
-		      uint64_t id, const uint32_t *ch, size_t len,
-		      unsigned int width,
-		      unsigned int posx, unsigned int posy,
-		      const struct tsm_screen_attr *attr)
+static void bblit_unset(struct kmscon_text *txt)
 {
+	struct bblit *bb = txt->data;
+
+	kmscon_rotate_free_tables(bb->glyphs, bb->bold_glyphs);
+}
+
+static int bblit_rotate(struct kmscon_text *txt, enum Orientation orientation)
+{
+	bblit_unset(txt);
+	txt->orientation = orientation;
+	return bblit_set(txt);
+}
+
+static int find_glyph(struct kmscon_text *txt, struct uterm_video_buffer **out,
+		      uint64_t id, const uint32_t *ch, size_t len, const struct tsm_screen_attr *attr)
+{
+	struct bblit *bb = txt->data;
+	struct uterm_video_buffer *bb_glyph;
 	const struct kmscon_glyph *glyph;
-	int ret;
+	struct shl_hashtable *gtable;
 	struct kmscon_font *font;
+	int ret;
+	bool res;
 
-	if (!width)
-		return 0;
-
-	if (attr->bold)
+	if (attr->bold) {
+		gtable = bb->bold_glyphs;
 		font = txt->bold_font;
-	else
+	} else {
+		gtable = bb->glyphs;
 		font = txt->font;
+	}
 
 	if (attr->underline)
 		font->attr.underline = true;
@@ -87,29 +141,100 @@ static int bblit_draw(struct kmscon_text *txt,
 	else
 		font->attr.italic = false;
 
-	if (!len) {
-		ret = kmscon_font_render_empty(font, &glyph);
-	} else {
-		ret = kmscon_font_render(font, id, ch, len, &glyph);
+	res = shl_hashtable_find(gtable, (void**)&bb_glyph, id);
+	if (res) {
+		*out = bb_glyph;
+		return 0;
 	}
+
+	bb_glyph = malloc(sizeof(*bb_glyph));
+	if (!bb_glyph)
+		return -ENOMEM;
+	memset(bb_glyph, 0, sizeof(*bb_glyph));
+
+	if (!len)
+		ret = kmscon_font_render_empty(font, &glyph);
+	else
+		ret = kmscon_font_render(font, id, ch, len, &glyph);
 
 	if (ret) {
 		ret = kmscon_font_render_inval(font, &glyph);
 		if (ret)
-			return ret;
+			goto err_free;
+	}
+
+	ret = kmscon_rotate_glyph( bb_glyph, glyph, txt->orientation, 1);
+	if (ret)
+		goto err_free;
+
+	ret = shl_hashtable_insert(gtable, id, bb_glyph);
+	if (ret)
+		goto err_free_vb;
+
+	*out = bb_glyph;
+	return 0;
+
+err_free_vb:
+	free(bb_glyph->data);
+err_free:
+	free(bb_glyph);
+	return ret;
+}
+
+
+static int bblit_draw(struct kmscon_text *txt,
+		      uint64_t id, const uint32_t *ch, size_t len,
+		      unsigned int width,
+		      unsigned int posx, unsigned int posy,
+		      const struct tsm_screen_attr *attr)
+{
+	struct uterm_video_buffer *bb_glyph;
+	struct uterm_mode *mode;
+	unsigned sw, sh, x, y, cwidth;
+	int ret;
+
+	if (!width)
+		return 0;
+
+	mode = uterm_display_get_current(txt->disp);
+	if (!mode)
+		return -EINVAL;
+	sw = uterm_mode_get_width(mode);
+	sh = uterm_mode_get_height(mode);
+
+	ret = find_glyph(txt, &bb_glyph, id, ch, len, attr);
+	if (ret)
+		return ret;
+
+	switch (txt->orientation) {
+	default:
+	case OR_NORMAL:
+		x = posx * FONT_WIDTH(txt);
+		y = posy * FONT_HEIGHT(txt);
+		break;
+	case OR_UPSIDE_DOWN:
+		cwidth = bb_glyph->width / FONT_WIDTH(txt);
+		x = sw - (posx + cwidth) * FONT_WIDTH(txt);
+		y = sh - (posy + 1) * FONT_HEIGHT(txt);
+		break;
+	case OR_RIGHT:
+		x = sw - (posy + 1) * FONT_HEIGHT(txt);
+		y = posx * FONT_WIDTH(txt);
+		break;
+	case OR_LEFT:
+		cwidth = bb_glyph->height / FONT_WIDTH(txt);
+		x = posy * FONT_HEIGHT(txt);
+		y = sh - (posx + cwidth) * FONT_WIDTH(txt);
+		break;
 	}
 
 	/* draw glyph */
 	if (attr->inverse) {
-		ret = uterm_display_fake_blend(txt->disp, &glyph->buf,
-					       posx * txt->font->attr.width,
-					       posy * txt->font->attr.height,
+		ret = uterm_display_fake_blend(txt->disp, bb_glyph, x, y,
 					       attr->br, attr->bg, attr->bb,
 					       attr->fr, attr->fg, attr->fb);
 	} else {
-		ret = uterm_display_fake_blend(txt->disp, &glyph->buf,
-					       posx * txt->font->attr.width,
-					       posy * txt->font->attr.height,
+		ret = uterm_display_fake_blend(txt->disp, bb_glyph, x, y,
 					       attr->fr, attr->fg, attr->fb,
 					       attr->br, attr->bg, attr->bb);
 	}
@@ -120,10 +245,11 @@ static int bblit_draw(struct kmscon_text *txt,
 struct kmscon_text_ops kmscon_text_bblit_ops = {
 	.name = "bblit",
 	.owner = NULL,
-	.init = NULL,
-	.destroy = NULL,
+	.init = bblit_init,
+	.destroy = bblit_destroy,
 	.set = bblit_set,
-	.unset = NULL,
+	.unset = bblit_unset,
+	.rotate = bblit_rotate,
 	.prepare = NULL,
 	.draw = bblit_draw,
 	.render = NULL,
