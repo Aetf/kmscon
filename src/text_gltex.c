203040023a113034b80d12efa6f0453c12cb2237
Jocelyn Falempe <jfalempe@redhat.com>
2025-10-22 10:57:23 +0200

    Move FONT_WIDTH()/FONT_HEIGHT() macro to text.h
    So that bblit and pixman renderer can use it too.
    
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>

--- src/text_gltex.c
+++ src/text_gltex.c
@@ -114,9 +114,6 @@ struct gltex {
 	unsigned int sh;
 };
 
-#define FONT_WIDTH(txt) ((txt)->font->attr.width)
-#define FONT_HEIGHT(txt) ((txt)->font->attr.height)
-
 static int gltex_init(struct kmscon_text *txt)
 {
 	struct gltex *gt;
