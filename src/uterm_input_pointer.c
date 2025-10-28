f749a31bc9bead3b49887410c938a69d2b4ac582
Jocelyn Falempe <jfalempe@redhat.com>
2025-10-28 13:44:03 +0100

    Add basic mouse support
    Start support for standard mouse, using relative EV_REL events
    
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>

---
+++ src/uterm_input_pointer.c
@@ -0,0 +1,75 @@
+#include <linux/input.h>
+#include "shl_hook.h"
+#include "shl_llog.h"
+#include "uterm_input.h"
+#include "uterm_input_internal.h"
+
+
+static void pointer_dev_send_move(struct uterm_input_dev *dev)
+{
+	struct uterm_input_pointer_event pev = {0};
+
+	pev.event = UTERM_MOVED;
+	pev.pointer_x = dev->pointer.x;
+	pev.pointer_y = dev->pointer.y;
+
+	shl_hook_call(dev->input->pointer_hook, dev->input, &pev);
+}
+
+static void pointer_dev_send_button(struct uterm_input_dev *dev, uint8_t button, bool pressed)
+{
+	struct uterm_input_pointer_event pev = {0};
+
+	pev.event = UTERM_BUTTON;
+	pev.button = button;
+	pev.pressed = pressed;
+
+	shl_hook_call(dev->input->pointer_hook, dev->input, &pev);
+}
+
+void pointer_dev_sync(struct uterm_input_dev *dev)
+{
+	struct uterm_input_pointer_event pev = {0};
+
+	pev.event = UTERM_SYNC;
+
+	shl_hook_call(dev->input->pointer_hook, dev->input, &pev);
+}
+
+void pointer_dev_rel(struct uterm_input_dev *dev,
+		      uint16_t code, int32_t value)
+{
+	switch (code) {
+	case REL_X:
+		dev->pointer.x += value;
+		pointer_dev_send_move(dev);
+		break;
+	case REL_Y:
+		dev->pointer.y += value;
+		pointer_dev_send_move(dev);
+		break;
+	default:
+		break;
+	}
+}
+
+void pointer_dev_button(struct uterm_input_dev *dev,
+			uint16_t code, int32_t value)
+{
+	bool pressed = (value == 1);
+
+	switch (code) {
+	case BTN_LEFT:
+		pointer_dev_send_button(dev, 0, pressed);
+		break;
+	case BTN_RIGHT:
+		pointer_dev_send_button(dev, 1, pressed);
+		break;
+	case BTN_MIDDLE:
+		pointer_dev_send_button(dev, 2, pressed);
+		break;
+	default:
+		break;
+	}
+}
+
