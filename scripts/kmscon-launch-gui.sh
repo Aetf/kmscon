Aetf/kmscon

commit 4c2893ac2680df415a75eb9ab538aa54fd08cf60

Add drmSetBackground and drmSetForeground escape codes

scripts/kmscon-launch-gui.sh
@@ -1,0 +1,10 @@
+#!/bin/sh
+
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 argument"
+    exit 1
+fi
+
+printf "\x1B]setBackground\a"
+eval "${@:1}"
+printf "\x1B]setForeground\a"

meson.build
@@ -191,6 +191,10 @@ foreach filename, kwargs : {
     'install_dir': bindir,
     'install_mode': 'rwxr-xr-x',
   },
+  'scripts/kmscon-launch-gui.sh': {
+    'install_dir': bindir,
+    'install_mode': 'rwxr-xr-x',
+  },
   'docs/kmscon.service.in': {
     'install_dir': systemdsystemunitdir,
     'install_mode': 'rw-r--r--',

src/kmscon_terminal.c
@@ -120,7 +120,7 @@ static void do_redraw_screen(struct screen *scr)
 {
 	int ret;
 
-	if (!scr->term->awake)
+	if (!scr->term->awake || !kmscon_session_get_foreground(scr->term->session))
 		return;
 
 	scr->pending = false;
@@ -201,6 +201,21 @@ static void display_event(struct uterm_display *disp,
 	scr->swapping = false;
 	if (scr->pending)
 		do_redraw_screen(scr);
+}
+
+static void osc_event(struct tsm_vte *vte, const char *osc_string,
+	size_t osc_len, void *data)
+{
+	struct kmscon_terminal *term = data;
+
+	if (strcmp(osc_string, "setBackground") == 0) {
+		log_info("Got OSC setBackground");
+		kmscon_session_set_background(term->session);
+	}
+	else if (strcmp(osc_string, "setForeground") == 0) {
+		log_info("Got OSC setForeground");
+		kmscon_session_set_foreground(term->session);
+	}
 }
 
 /*
@@ -638,6 +653,8 @@ int kmscon_terminal_register(struct kmscon_session **out,
 
 	tsm_vte_set_backspace_sends_delete(term->vte,
 					   BUILD_BACKSPACE_SENDS_DELETE);
+
+	tsm_vte_set_osc_cb(term->vte, osc_event, (void *)term);
 
 	ret = tsm_vte_set_palette(term->vte, term->conf->palette);
 	if (ret)

commit 36efdb9c3183220f060822176c65435c821aba31

kmscon-launch-gui: don't use eval
this eval is harmful as it makes it necessary to add one more level of escaping
to the arguments.

Note this could break existing users, but given the script is recent I
believe it is still worth improving.
If eval-like behaviour is required one can run `sh -c ...` for identical
effect:
```
$ cat /tmp/t.sh
#!/bin/sh

echo --- no eval
"$@"
echo --- eval
eval "$@"
echo ---------------
$ /tmp/t.sh printf "%s\n" "test space"
--- no eval
test space
--- eval
testnspacen---------------
$ /tmp/t.sh sh -c 'printf "%s\n" "test space"'
--- no eval
test space
--- eval
printf: usage: printf [-v var] format [arguments]
---------------
```

Fixes: 4c2893ac2680 ("Add drmSetBackground and drmSetForeground escape codes")
Signed-off-by: Dominique Martinet <asmadeus@codewreck.org>

scripts/kmscon-launch-gui.sh
@@ -16,7 +16,7 @@ else
     printf "\033Ptmux;\033\033]setBackground\a\033\\"
 fi
 
-eval "$@"
+"$@"
 
 # If the current tty has changed, wait until the user switches back.
 if [ -n "${kms_tty}" ]; then